5833b5469c07f4799b05a20ea3c039ff3bf496aa
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-07-22 02:42:03 -0300

    rentals route unit tests

--- tests/integration/rentals.test.ts
+++ tests/integration/rentals.test.ts
@@ -1,5 +1,192 @@
-describe("Rentals Service Unit Tests", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
-  })
+import supertest from "supertest";
+import app from "../../src/app";
+import { cleanDb } from "../utils";
+import { buildUser } from "../factories/user-factory";
+import { buildMovie } from "../factories/movie-factory";
+import { buildRental, rentMovie } from "../factories/rentals-factory";
+import { RentalFinishInput, RentalInput } from "../../src/protocols";
+
+const api = supertest(app);
+
+beforeEach( async () => {
+  await cleanDb();
 })
+
+describe("GET /rentals", () => {
+  it("should return all rentals", async () => {
+    const user = await buildUser();
+    const movie = await buildMovie();
+    const rental = await buildRental(user.id);
+    await rentMovie(rental.id, movie.id);
+
+    const { status, body } = await api.get("/rentals");
+    expect(status).toBe(200);
+    expect(body).toEqual([{
+      id: rental.id,
+      date: expect.any(String),
+      endDate: expect.any(String),
+      userId: user.id,
+      closed: false
+    }]);
+  });
+})
+
+describe("GET /rentals/:id", () => {
+  it("should return all rentals", async () => {
+    const user = await buildUser();
+    const movie = await buildMovie();
+    const rental = await buildRental(user.id);
+    await rentMovie(rental.id, movie.id);
+
+    const { status, body } = await api.get(`/rentals/${rental.id}`);
+    expect(status).toBe(200);
+    expect(body).toEqual({
+      id: rental.id,
+      date: expect.any(String),
+      endDate: expect.any(String),
+      userId: user.id,
+      closed: false
+    });
+  });
+
+  it("should return 400 when id is invalid", async () => {
+    const { status } = await api.get(`/rentals/invalid`);
+    expect(status).toBe(400);
+  });
+
+  it("should return 404 when rental does not exist", async () => {
+    const { status } = await api.get(`/rentals/1`);
+    expect(status).toBe(404);
+  });
+
+});
+
+describe("POST /rentals", () => {
+  it("should return all rentals", async () => {
+    const user = await buildUser();
+    const movie = await buildMovie();
+    const rental: RentalInput = {
+      userId: user.id,
+      moviesId: [movie.id]
+    } 
+    
+    const { status } = (await api.post(`/rentals`).send(rental));
+    expect(status).toBe(201);
+  });
+
+  it("should return 422 when data is missing", async () => {
+    type RentalInputMissingData = Omit<RentalInput, "moviesId">;
+    const rentalMissing : RentalInputMissingData = {
+      userId: 1
+    }
+    /*const user = await buildUser();
+    const movie = await buildMovie();
+    const rentalMissing: RentalInputMissingData = {
+      userId: user.id
+    } */
+
+    const { status } = await api.post(`/rentals`).send(rentalMissing);
+    expect(status).toBe(422);
+  });
+
+  it("should return 404 when id user does not exist", async () => {
+    //const user = await buildUser(); //não criar user
+    const movie = await buildMovie();
+    const rental: RentalInput = {
+      userId: 1,                      //tenta puxar o id do user que não existe
+      moviesId: [movie.id]
+    } 
+    const { status } = await api.post(`/rentals`).send(rental);
+    expect(status).toBe(404);
+  });
+
+  it("should return 401 when movie does not exist", async () => {
+    const user = await buildUser(); 
+    //const movie = await buildMovie();     //não criar movie
+    const rental: RentalInput = {
+      userId: user.id,                      
+      moviesId: [1]                         //tenta puxar o id do movie que não existe
+    } 
+    const { status } = await api.post(`/rentals`).send(rental);
+    expect(status).toBe(404);
+  });
+  
+  it("should return 401 when a minor tries to rental a movie that is adult only", async () => {
+    const user = await buildUser(false);    //criança
+    const movie = await buildMovie(true);   //filme para adulto
+    const rental: RentalInput = {
+      userId: user.id,
+      moviesId: [movie.id]
+    } 
+    
+    const { status } = (await api.post(`/rentals`).send(rental));
+    expect(status).toBe(401);
+  });
+
+  it("should return 409 when user tries to rent a movie that is already in another rental", async () => {
+    const user = await buildUser();    
+    const movie = await buildMovie();   
+    const rental = await buildRental(user.id) 
+    await rentMovie(rental.id, movie.id);     //user1 rental movie
+
+    const user2 = await buildUser();          //cria um novo usuario para tentar alugar o filme já alugado
+    const rentalAtemp : RentalInput = {
+      userId:user2.id,
+      moviesId: [movie.id]
+    }
+
+    const { status } = (await api.post(`/rentals`).send(rentalAtemp)); 
+    expect(status).toBe(409);
+  });
+
+  it("should return 402 when user already have a rental to return", async () => {
+    const user = await buildUser();    
+    const movie = await buildMovie();   
+    const rental = await buildRental(user.id) 
+    await rentMovie(rental.id, movie.id);     
+
+    const movie2 = await buildMovie();          //cria um novo filme para tentar alugar
+    const rentalAtemp : RentalInput = {
+      userId:user.id,
+      moviesId: [movie2.id]
+    };
+
+    const { status } = (await api.post(`/rentals`).send(rentalAtemp)); 
+    expect(status).toBe(402);
+  });
+
+});
+
+describe("POST /rentals/finish", () => {
+  it("should return 422 when data is missing", async () => {
+    const { status } = (await api.post(`/rentals/finish`).send({}));
+    expect(status).toBe(422);
+  });
+
+  it("should finalize a valid rentals", async () => {
+    const user = await buildUser();
+    const movie1 = await buildMovie();
+    const movie2 = await buildMovie();
+    const rental = await buildRental(user.id);
+
+    await rentMovie(rental.id, movie1.id);
+    await rentMovie(rental.id, movie2.id);
+    
+    const data: RentalFinishInput = {
+      rentalId: rental.id
+    };
+
+    const { status } = (await api.post(`/rentals/finish`).send(data));
+    expect(status).toBe(200);
+  });
+
+  it("should return 404 when rental does not exist", async () => {
+    const data: RentalFinishInput = {
+      rentalId: 1
+    };
+
+    const { status } = (await api.post(`/rentals/finish`).send(data));
+    expect(status).toBe(404);
+  });
+
+});
--- tests/unit/rentals-unit.test.ts
+++ tests/unit/rentals-unit.test.ts
@@ -1,6 +1,12 @@
 import rentalsService from "../../src/services/rentals-service";
 import rentalsRepository from "../../src/repositories/rentals-repository";
+import { Movie, Rental, User } from "@prisma/client";
 import { notFoundError } from "../../src/errors/notfound-error";
+import { buildUserInput } from "../factories/user-factory";
+import { buildRentalReturn } from "../factories/rentals-factory";
+import usersRepository from "../../src/repositories/users-repository";
+import { buildMovieInput } from "../factories/movie-factory";
+import moviesRepository from "../../src/repositories/movies-repository";
 
 describe("Rentals Service Unit Tests", () => {
 
@@ -8,40 +14,204 @@ describe("Rentals Service Unit Tests", () => {
     jest.clearAllMocks();
   })
 
-  it("should return rentals", async () => {
-    jest.spyOn(rentalsRepository, "getRentals").mockResolvedValueOnce([
-      { id: 1, closed:false, date: new Date(), endDate: new Date(), userId:1 },
-      { id: 2, closed:false, date: new Date(), endDate: new Date(), userId:2 }
-    ])
+  describe("Get rentals tests", () => {
+    it("should return rentals", async () => {
+      jest.spyOn(rentalsRepository, "getRentals").mockResolvedValueOnce([
+        { id: 1, closed:false, date: new Date(), endDate: new Date(), userId:1 },
+        { id: 2, closed:false, date: new Date(), endDate: new Date(), userId:2 }
+      ]);
+  
+      const rentals = await rentalsService.getRentals();
+      expect(rentals).toHaveLength(2);
+    });
+  
+    it("should return rentals by rentalId", async () => {
+      //no exemplo é uma dulto alugando filme de adulto (sucesso)
+      const mockRental: Rental & { movies: Movie[] }= { 
+        id: 1, 
+        closed: false, 
+        date: new Date(), 
+        endDate: new Date(), 
+        userId:1, 
+        movies: [
+          {
+            id: 1,
+            name: "Panico IV",
+            adultsOnly: true,
+            rentalId: 1
+          }
+        ]
+      }
+  
+      jest.spyOn(rentalsRepository, "getRentalById").mockResolvedValueOnce(mockRental);
+  
+      const rental = await rentalsService.getRentalById(1);
+      expect(rental).toEqual(mockRental);
+    });
+  
+    it("shoul return notFoundError when specifc retal is not found", async () => {
+      jest.spyOn(rentalsRepository, "getRentalById").mockResolvedValueOnce(null); //obs que estamos mandando nullo para dar erro
+  
+      const promisse = rentalsService.getRentalById(1); //colocamos id 1 mas ele não foi criado, além disso não é necessário o uso do await já que não estamos realmente esperando uma resposta e é para dar erro.
+  
+      //dai como é esperado o erro já usa o rejects
+      expect(promisse).rejects.toEqual(notFoundError("Rental not found."));
+    });
 
-    const rentals = await rentalsService.getRentals();
-    expect(rentals).toHaveLength(2);
-  })
-
-  it("should return rentals by rentalId", async () => {
-    const rentalId = 9
-    const mockRental = { 
-      id: 1, 
-      closed: false, 
-      date: new Date(), 
-      endDate: new Date(), 
-      userId:1, 
-      movies: [
-        {
-          id: 1,
-          name: "Sooby Doo",
-          adultsOnly: false,
-          rentalId: 1
-        }
-      ]
-    }
-
-    jest.spyOn(rentalsRepository, "getRentalById").mockResolvedValueOnce(mockRental);
-
-    const rental = await rentalsService.getRentalById(rentalId);
-
-    expect(rentalsRepository.getRentalById).toHaveBeenCalledWith(rentalId);
-    expect(rental).toEqual(mockRental);
   });
 
-})
+  describe("Create rentalks tests", () => {
+
+    it("shoul throw an error when user does not exist", async () => {
+      jest.spyOn(usersRepository, "getById").mockResolvedValueOnce(null);
+
+      const promisse = rentalsService.createRental({
+        userId: 1,
+        moviesId: [ 1, 2, 3, 4]
+      });
+
+      expect(promisse).rejects.toEqual({
+        name: "NotFoundError",
+        message: "User not found."
+      });
+    });
+
+    it("shoul throw an error when user alredy have a rental", async () => {
+      const mockUser: User = { id: 1,  ...buildUserInput(true) };
+      const mockUserRental: Rental = buildRentalReturn(1, true);
+      
+      jest.spyOn(usersRepository, "getById").mockResolvedValueOnce(mockUser);
+      jest.spyOn(rentalsRepository, "getRentalsByUserId").mockResolvedValue([mockUserRental]);
+
+      const promise = rentalsService.createRental({
+        userId: mockUserRental.id,
+        moviesId: [4, 5, 6, 7]
+      });
+
+      expect(promise).rejects.toEqual({
+        name: "PendentRentalError",
+        message: "The user already have a rental!"
+      });
+    });
+
+    it("shoul throw an error when a minor user wants to rent a adult only movie", async () => {
+      const mockUser: User = { id: 1,  ...buildUserInput(false) };
+      const mockMovie: Movie = { 
+        id: 1,
+        rentalId: null,
+        ...buildMovieInput(true)
+      }
+
+      jest.spyOn(usersRepository, "getById" ).mockResolvedValue(mockUser);
+      jest.spyOn(rentalsRepository, "getRentalsByUserId").mockResolvedValue([]);
+      jest.spyOn(moviesRepository, "getById").mockResolvedValue(mockMovie);
+
+      const promise = rentalsService.createRental({
+        userId: mockUser.id,
+        moviesId: [1]
+      })
+
+      expect(promise).rejects.toEqual({
+        name: "InsufficientAgeError",
+        message: "Cannot see that movie."
+      })
+    });
+
+    it("shoul throw an error when a movie does not exist", async () => {
+      const mockUser: User = { id: 1,  ...buildUserInput(true) };
+  
+      jest.spyOn(usersRepository, "getById" ).mockResolvedValue(mockUser);
+      jest.spyOn(rentalsRepository, "getRentalsByUserId").mockResolvedValue([]);
+      jest.spyOn(moviesRepository, "getById").mockResolvedValue(null);
+  
+      const promise = rentalsService.createRental({
+        userId: mockUser.id,
+        moviesId: [1]
+      });
+  
+      expect(promise).rejects.toEqual({
+        name: "NotFoundError",
+        message: "Movie not found."
+      });
+    });
+
+    it("should throw an error when movie is not available", async () => {
+      const mockUser: User = { id: 1,  ...buildUserInput(true) };
+      const mockMovie: Movie = { 
+        id: 1,
+        rentalId: 1,
+        ...buildMovieInput(true)
+      }
+
+      jest.spyOn(usersRepository, "getById" ).mockResolvedValue(mockUser);
+      jest.spyOn(rentalsRepository, "getRentalsByUserId").mockResolvedValue([]);
+      jest.spyOn(moviesRepository, "getById").mockResolvedValue(mockMovie);
+
+      const promise = rentalsService.createRental({
+        userId: 1,
+        moviesId: [1]
+      })
+
+      expect(promise).rejects.toEqual({
+        name: "MovieInRentalError",
+        message: "Movie already in a rental."
+      })
+    })
+
+    it("shoul do a rental for a movie", async () => {
+      //(sucesso)
+      const mockUser: User = { id: 1,  ...buildUserInput(true) };
+      const mockMovie: Movie = { 
+        id: 1,
+        rentalId: null,
+        ...buildMovieInput(true),
+      }
+
+      jest.spyOn(usersRepository, "getById" ).mockResolvedValue(mockUser);
+      jest.spyOn(rentalsRepository, "getRentalsByUserId").mockResolvedValue([]);
+      jest.spyOn(moviesRepository, "getById").mockResolvedValue(mockMovie);
+      jest.spyOn(rentalsRepository, "createRental").mockResolvedValue(null);
+
+      await rentalsService.createRental({
+        userId: mockUser.id,
+        moviesId: [1]
+      });
+
+    });
+
+  });  
+
+  describe("finish rentals test", () => {
+    it("shoul trhow an error if rental does not exists", async () => {
+      jest.spyOn(rentalsRepository, "getRentalById").mockResolvedValue(null);
+      
+      const promise = rentalsService.finishRental(1);
+
+      expect(promise).rejects.toEqual({
+        name: "NotFoundError",
+        message: "Rental not found."
+      });
+    });
+
+    it("shoud finish a rental", async () => {
+      //sucesso
+      const mockRental: Rental & { movies: Movie[] } = {
+        ...buildRentalReturn(1, false),
+        movies: [
+          {
+            id:1,
+            rentalId: 1,
+            ...buildMovieInput(true)
+          }
+        ]
+      }
+      jest.spyOn(rentalsRepository, "getRentalById").mockResolvedValue(mockRental);
+      jest.spyOn(rentalsRepository, "finishRental").mockResolvedValue();
+
+      await rentalsService.finishRental(1);
+    });
+
+  });
+
+
+});
